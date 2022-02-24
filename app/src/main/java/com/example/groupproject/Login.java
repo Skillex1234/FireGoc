0d19c77cd4eb943f0a5b2ed46b20fcb7c75a68c0
Owner <mmaldj10@farmingdale.edu>
2022-02-24 10:00:07 -0500

    first commit

---
+++ app/src/main/java/com/example/groupproject/Login.java
@@ -0,0 +1,46 @@
+package com.example.groupproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        final EditText user = findViewById(R.id.user);
+        final EditText pass = findViewById(R.id.pass);
+        final Button signin = findViewById(R.id.signin);
+        final TextView register = findViewById(R.id.register);
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final String usertext = user.getText().toString();
+                final String passtext = pass.getText().toString();
+
+                if(usertext.isEmpty() || passtext.isEmpty())
+                {
+                    Toast.makeText(Login.this, "please enter your username or password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this, Register.class));
+
+            }
+        });
+    }
+}
