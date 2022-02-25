2e93e72a3037b44a9a7ab58f6cf8a053445a72d4
Joseph Maldonado <Malgon07@gmail.com>
2022-02-24 20:56:38 -0500

    updated sign up / login

--- app/src/main/java/com/example/groupproject/Login.java
+++ app/src/main/java/com/example/groupproject/Login.java
@@ -1,7 +1,7 @@
 package com.example.groupproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,18 +9,24 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Login extends AppCompatActivity {
 
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://fire-groceries-514d0-default-rtdb.firebaseio.com/");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         EditText user = findViewById(R.id.user);
         EditText pass = findViewById(R.id.pass);
         Button signin = findViewById(R.id.signin);
         TextView register = findViewById(R.id.register);
+
         signin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,7 +39,32 @@ public class Login extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "please enter your username or password", Toast.LENGTH_SHORT).show();
                 }
                 else{
-                    startActivity(new Intent(Login.this, HomeScreen.class));
+                    databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if(snapshot.hasChild(usertext)){
+                                final String getpassword = snapshot.child(usertext).child("password").getValue(String.class);
+
+                                if(getpassword.equals(usertext)){
+                                    Toast.makeText(Login.this, "Successfully logged in", Toast.LENGTH_SHORT).show();
+
+                                    startActivity(new Intent(Login.this, HomeScreen.class));
+                                    finish();
+                                }
+                                else{
+                                    Toast.makeText(Login.this, "Incorrect password", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                            else{
+                                Toast.makeText(Login.this, "Wrong username", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
                 }
             }
         });
@@ -41,7 +72,7 @@ public class Login extends AppCompatActivity {
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(Login.this, Register.class));
+                startActivity(new Intent(Login.this, SignUp.class));
 
             }
         });
--- app/src/main/java/com/example/groupproject/SignUp.java
+++ app/src/main/java/com/example/groupproject/SignUp.java
@@ -1,27 +1,33 @@
 package com.example.groupproject;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SignUp extends AppCompatActivity {
 
-
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://fire-groceries-514d0-default-rtdb.firebaseio.com/");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up);
 
         final EditText user = findViewById(R.id.user);
         final EditText email = findViewById(R.id.email);
         final EditText phone = findViewById(R.id.phone);
         final EditText password = findViewById(R.id.password);
         final EditText confirm_password = findViewById(R.id.confirm_pass);
-
         final Button signup = findViewById(R.id.signup);
 
 
@@ -37,12 +43,42 @@ public class SignUp extends AppCompatActivity {
                     if(userTxt.isEmpty()||emailTxt.isEmpty()|| phoneTxt.isEmpty()||passwordTxt.isEmpty()||confirm_passwordTxt.isEmpty()){
                         Toast.makeText(getApplicationContext(), "Please fill all the fields", Toast.LENGTH_SHORT).show();
                     }
-                    else if(!phoneTxt.equals(confirm_passwordTxt)){
+                    else if(!passwordTxt.equals(confirm_passwordTxt)){
                         Toast.makeText(getApplicationContext(), "Passwords not matching", Toast.LENGTH_SHORT).show();
                     }
                     else {
 
+                        databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                if(snapshot.hasChild(userTxt)){
+                                    Toast.makeText(SignUp.this, "this username is already used", Toast.LENGTH_SHORT).show();
+                                }
+                                else{
+                                    databaseReference.child("users").child(userTxt).child("username").setValue(userTxt);
+                                    databaseReference.child("users").child(userTxt).child("email").setValue(emailTxt);
+                                    databaseReference.child("users").child(userTxt).child("password").setValue(passwordTxt);
+                                    databaseReference.child("users").child(userTxt).child("phone").setValue(phoneTxt);
+
+                                    Toast.makeText(SignUp.this, "user registered", Toast.LENGTH_SHORT).show();
+                                    finish();
+                                }
+
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
+
                     }
+                }
+            });
+            signup.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    finish();
                 }
             });
 
