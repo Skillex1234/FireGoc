6369eb74d05cacb9ba6466ee8d661dff9abaf9f8
Richie <rrueck13@gmail.com>
2022-02-28 17:12:17 -0500

    Updated Home Screen

--- app/src/main/java/com/example/groupproject/HomeScreen.java
+++ app/src/main/java/com/example/groupproject/HomeScreen.java
@@ -1,16 +1,27 @@
 package com.example.groupproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeScreen extends AppCompatActivity {
+
+    ImageView Coke;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
-
+        Coke = findViewById(R.id.imageViewCoke);
+        Coke.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeScreen.this, Login.class));
+            }
+        });
     }
 }
