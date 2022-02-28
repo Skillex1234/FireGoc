27aff0acd6cded8967e9f990dd6b282cc06146ed
Joseph Maldonado <Malgon07@gmail.com>
2022-02-28 00:23:10 -0500

    login meat category code added

--- app/src/main/java/com/example/groupproject/HomeScreen.java
+++ app/src/main/java/com/example/groupproject/HomeScreen.java
@@ -1,16 +1,28 @@
 package com.example.groupproject;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class HomeScreen extends AppCompatActivity {
+
+    Button meatcategory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
+        meatcategory = findViewById(R.id.buttonCategoryMeats);
 
+        meatcategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeScreen.this, MeatsCategory.class));
+            }
+        });
     }
 }
