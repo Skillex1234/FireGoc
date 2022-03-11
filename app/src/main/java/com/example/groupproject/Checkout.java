f7f4ee982793b100e9f73b7ebdcc4d4c0fcbc02a
saadsyedquadri <saadsq23@gmail.com>
2022-03-11 17:03:26 -0500

    updating checkout pages

--- app/src/main/java/com/example/groupproject/Checkout.java
+++ app/src/main/java/com/example/groupproject/Checkout.java
@@ -2,13 +2,27 @@ package com.example.groupproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Checkout extends AppCompatActivity {
+
+
+    Button itemPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+
+
+        itemPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Checkout.this, ItemPage.class));
+            }
+        });
     }
 }
