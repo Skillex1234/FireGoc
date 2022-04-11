723a74a3a298df05b850077efea3679ea2ba72cc
saadsyedquadri <saadsq23@gmail.com>
2022-04-11 01:59:26 -0400

    updated payment files, with layout of form

--- app/src/main/java/com/example/groupproject/Payment.java
+++ app/src/main/java/com/example/groupproject/Payment.java
@@ -8,37 +8,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 public class Payment extends AppCompatActivity {
 
-    ListView listView;
+    EditText cardNumber, expirationDate,CvvNumber,CardHolderName;
+    //Button sumbitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.payment);
 
-        listView = findViewById(R.id.listView);
-
-        String [] values = new String[]{
-                "Mastercard ending 2012, EXP: 2/25", "Visa ending 3423 EXP: 3/23"
-        };
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, values);
-
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int postion, long id) {
-                if (postion == 0) {
-                    Intent intent = new Intent(view.getContext(), MainActivity.class);
-                    startActivity(intent);
-                }
-            }
-        });
     }
 }
 
